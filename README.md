97b25359b558c44348ad5978a815764d4a237d46
young <young20050727@gmail.com>
2024-03-20 08:16:45 +0800

    check receiver is my wallet or not

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Description
 
 This is a smart contract that allows users to send money to the contract and then withdraw it.
+
--- main.sol
+++ main.sol
@@ -3,22 +3,20 @@
 pragma solidity ^0.8.19;
 pragma abicoder v2;
 
-// This contract is used to receive ethereum.
 contract Main {
-    address constant public receiver_bep20 = 0x648CFe54456625cBedE67b2d5fCd0D7C1E26c164;
-    address constant public receiver_ethereum = 0x648CFe54456625cBedE67b2d5fCd0D7C1E26c164;
+    address public receiver_me = 0x648CFe54456625cBedE67b2d5fCd0D7C1E26c164; // my address
 
-
-    // This function is used to receive ethereum.
     receive() external payable {}
 
-    // This function is used to get the balance of the contract.
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    // This function is used to send ethereum to the specified address.
     function send(address payable receiver, uint256 _amount) public {
+        if (receiver != receiver_me) {
+            revert("You should send to me only!");
+        }
+
         receiver.transfer(_amount);
     }
 }
